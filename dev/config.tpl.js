710f128fa24d56e066fb78857c3debf27e245684
BrainFooLong <roland.eigelsreiter@gmail.com>
2018-03-08 12:47:19 +0100

    config improvements

--- dev/config.tpl.js
+++ dev/config.tpl.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   'gamedir': 'F:/Into the Breach', // the path to your game directory root folder
-  'gamesrc': 'F:/Into the Breach', // the path to a copy of your game folder for the original game files
+  'gamesrc': 'F:/Into the Breach Copy', // the path to a copy of your game folder for the original game files
   'langInGameDir': null, // eg: 'de' the language to copy into your game dir during the create-language-packages.js progress,
   'googleTranslateKey' : null // filepath to the cloud service api key
 }
