1bd699dba1e3f74439b3ea0c143b44ce8708da64
Andrea Difonzo <adifonzo97@gmail.com>
2018-03-17 16:46:52 +0100

    Translated Conveyor Belts dialogue

--- dev/config.tpl.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-module.exports = {
-  'gamedir': 'F:/Into the Breach', // the path to your game directory root folder
-  'gamesrc': 'F:/Into the Breach Copy', // the path to a copy of your game folder for the original game files
-  'langInGameDir': null, // eg: 'de' the language to copy into your game dir during the create-language-packages.js progress,
-  'googleTranslateKey': null, // filepath to the cloud service api key, nothing to worry about, just for tests only
-  'editorPort': 6161, // if you use the editor-server method you can specify the port
-  'editorLanguage': 'de' // than language you want to edit with the editor
-}
